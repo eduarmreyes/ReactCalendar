a45117cad04b51da75c89101eb9250ba78ef6e5c
eduarmreyes <eduar.mreyes@gmail.com>
2017-07-28 14:33:41 -0600

    Calendar appointments with jQuery and Haml

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,20 @@
 class AppointmentsController < ApplicationController
   def index
+  	@appointment = Appointment.new
+  	@appointments = get_appointments_ordered_by_appt_time
+  end
+
+  def create
+  	@appointment = Appointment.create(appointment_params)
+  	@appointments = get_appointments_ordered_by_appt_time
+  end
+
+  private
+  def appointment_params
+  	params.require(:appointment).permit(:title, :appt_time)
+  end
+
+  def get_appointments_ordered_by_appt_time
+  	Appointment.order('appt_time ASC')
   end
 end
